64f9fcf7e43e914aa4d11c89523961c00222c2dc
Jul-Me <memai.julian@gmail.com>
2022-01-01 18:58:16 +0000

    Fetch user data from Github API

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+let user = null;
+
 function toggle() {
   const themeIconEl = document.querySelector('.theme-icon');
   const bodyEl = document.querySelector('body');
@@ -9,4 +11,13 @@ function toggle() {
     themeIconEl.innerText = 'dark_mode';
     bodyEl.classList.add('lightMode');
   }
+}
+
+async function searchUser() {
+  const searchValue = document.querySelector('.search-field').value;
+
+  const response = await fetch(`https://api.github.com/users/${searchValue}`);
+  user = await response.json();
+
+  console.log('data', user);
 }
