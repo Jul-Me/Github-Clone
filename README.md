172258f9d5f40a22c336373a0a871934865bdf38
Jul-Me <memai.julian@gmail.com>
2022-01-01 21:40:33 +0000

    Append user bio data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Github-Clone
+# Github-DevFinder
+
+Github DevFinder is a personal project built in Vanilla JavaScript and CSS. Using the public Github API to find and display its user's profile information. Search and find your favorite developer!
--- public/main.js
+++ public/main.js
@@ -1,5 +1,3 @@
-let user = null;
-
 function toggle() {
   const themeIconEl = document.querySelector('.theme-icon');
   const bodyEl = document.querySelector('body');
@@ -14,10 +12,42 @@ function toggle() {
 }
 
 async function searchUser() {
-  const searchValue = document.querySelector('.search-field').value;
+  const searchEl = document.querySelector('.search-field');
+  const searchValue = searchEl.value;
 
   const response = await fetch(`https://api.github.com/users/${searchValue}`);
-  user = await response.json();
+  const data = await response.json();
 
-  console.log('data', user);
+  console.log('data', data);
+
+  if (data) {
+    appendUserBio(data);
+    // appendUserStats(data);
+    // appendUserLinks(data);
+  }
+}
+
+function appendUserBio(user) {
+  const { avatar_url, name, login, created_at, html_url, bio } = user;
+
+  const avatarEl = document.querySelector('#avatar');
+  avatarEl.src = avatar_url;
+
+  const nameEl = document.querySelector('#name');
+  nameEl.innerText = name || login;
+
+  const joinedEl = document.querySelector('#joined');
+  const joinedDate = new Date(created_at);
+  const year = joinedDate.getFullYear();
+  const month = joinedDate.toLocaleString('default', { month: 'long' });
+  const day = joinedDate.getDate();
+  joinedEl.innerText = `Joined ${day} ${month} ${year}`;
+
+  const usernameEl = document.querySelector('#username');
+  usernameEl.innerText = login;
+  usernameEl.href = html_url;
+  usernameEl.target = '_blank';
+
+  const bioEl = document.querySelector('#bio');
+  bioEl.innerText = bio || 'This profile has no bio';
 }
